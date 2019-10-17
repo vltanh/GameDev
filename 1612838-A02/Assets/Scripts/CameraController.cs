064b5a265a3a7c43bc38fca546842533f72f1a26
vltanh <vltheanh030698@gmail.com>
2019-10-17 00:11:04 -0500

    added assignment 2

---
+++ 1612838-A02/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform followObj;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = followObj.transform.position;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        smoothedPosition.z = transform.position.z;
+        transform.position = smoothedPosition;
+    }
+}
---
+++ 1612838-A02/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public KeyCode moveLeft = KeyCode.LeftArrow,
+                moveRight = KeyCode.RightArrow,
+                moveUp = KeyCode.UpArrow,
+                moveDown = KeyCode.DownArrow,
+                attack = KeyCode.Space;
+
+    public float xVelocity = 5.0f,
+                yVelocity = 300.0f;
+
+    Animator anim;
+    Rigidbody2D rigidBody;
+    GameObject leg;
+
+    int jumpCount = 0;
+    int maxJumpTimes = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        leg = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(moveRight))
+        {
+            anim.Play("CharacterWalkFront");
+            rigidBody.AddForce(new Vector2(xVelocity, 0.0f));
+        }
+
+        if (Input.GetKey(moveLeft))
+        {
+            anim.Play("CharacterWalkBack");
+            rigidBody.AddForce(new Vector2(-xVelocity, 0.0f));
+        }
+
+        if (Input.GetKey(moveDown))
+        {
+            anim.Play("CharacterSit");
+        }
+
+        if (Input.GetKeyDown(moveUp))
+        {
+            if (jumpCount < maxJumpTimes)
+            {
+                anim.Play("CharacterJump");
+                rigidBody.AddForce(new Vector2(0.0f, yVelocity));
+                jumpCount += 1;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D myCollider = collision.contacts[0].otherCollider;
+        Collider2D otherCollider = collision.contacts[0].collider;
+        if (otherCollider.tag == "Ground" && myCollider.name == "CharacterLeg")
+        {
+            jumpCount = 0;
+        }
+    }
+}
